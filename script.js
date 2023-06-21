28bc3b05b4108b8d1fc2c2cb72f5e02f13cc4adc
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-21 16:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let prom1 = new Promise((resolve,reject)=>{
+	resolve("")
+},(Math.floor(Math.random() * 3000)))
+
+let prom2 = new Promise((resolve,reject)=>{
+	resolve("")
+},(Math.floor(Math.random() * 3000)))
+
+let prom3 = new Promise((resolve,reject)=>{
+	resolve("")
+},(Math.floor(Math.random() * 3000)))
+
+window.promises = [prom1, prom2, prom3 ];
+Promise.all	(promises)
+.then((value)=>{
+	document.getElementById("output").innerText = value ;
+})
+.catch((err)=>{
+	console.log("error");
